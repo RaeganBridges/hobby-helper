685d0173dfdb8e36b369c789e6bbe964fe7b4837
RaeganBridges <rbridge9@vols.utk.edu>
2026-03-29 21:18:38 -0400

    update

--- public/video-embed.js
+++ public/video-embed.js
@@ -1,6 +1,6 @@
 /**
  * Open a full-screen embedded MP4 player on the current page (no navigation).
- * Usage: openHobbyVideoEmbed({ src: '/games.mp4', closeStroke: '#e86775' });
+ * Usage: openHobbyVideoEmbed({ src: '/games.mp4', closeStroke: '#ea556c' });
  */
 (function () {
   var root, video, wrap, shell, frame, closeBtn, pathEl;
